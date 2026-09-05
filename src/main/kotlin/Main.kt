5302fa63a94058e7108f3865f6ef49bbb8eee89b
tor <tornanar@gmailc.om>
2026-09-05 20:07:30 +0300

    commit Added a comment on the fifteenth line

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,5 +12,6 @@ fun main() {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
+        // Тут я сделал изменения
     }
 }
